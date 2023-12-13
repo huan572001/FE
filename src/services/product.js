d4913a6e2b57e5929f17af156f7871b19f182af4
Dinh Nho Nam <nhonamstg@gmail.com>
2023-12-13 14:07:09 +0700

    product all

--- src/services/product.js
+++ src/services/product.js
@@ -3,7 +3,7 @@ import axiosClient from "./axiosClient";
 
 export const ProductAPI = {
   getAllProduct: async (data) => {
-    const url = `product/allProduct`;
+    const url = `product/allProduct-admin`;
     return axiosClient.get(url);
   },
   createProduct: async (data) => {
